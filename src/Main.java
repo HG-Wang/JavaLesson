d772ad1d435b13378d6bd979395eaa16c9e70b18
HG_Wang <cnhgwang@gmail.com>
2024-09-19 11:20:31 +0800

    删除杂物, 规范目录

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    {
-        System.out.println("This is an instance initializer block");
-    }
-    public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println("Hello, World!");
-    }
-}
